0dc8f9256e578b842a28a760032d7e02a69e9752
Maxzcv <59911607+Mr101003@users.noreply.github.com>
2024-06-30 09:10:37 +0700

    error  auth

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,4 +1,16 @@
 <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('title')</title>
+    <!-- Menyertakan file CSS -->
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <!-- Tambahan CSS lain jika ada -->
+    @yield('styles')
+</head>
+<body>
+{{-- <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
@@ -15,10 +27,10 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100">
+        <div class="min-h-screen bg-gray-100"> --}}
             @include('layouts.navigation')
 
-            <!-- Page Heading -->
+            {{-- <!-- Page Heading -->
             @if (isset($header))
                 <header class="bg-white shadow">
                     <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
@@ -31,6 +43,16 @@
             <main>
                 {{ $slot }}
             </main>
+        </div> --}}
+        <div class="container">
+            @yield('content')
         </div>
+    
+        @include('partials.footer')
+    
+        <!-- Menyertakan file JavaScript -->
+        <script src="{{ asset('js/script.js') }}"></script>
+        <!-- Tambahan JavaScript lain jika ada -->
+        @yield('scripts')
     </body>
 </html>
